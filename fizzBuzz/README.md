96492ea520ec9f9dd4012a8f399300ca875ea42c
ruthearle <rrearle@gmail.com>
2014-08-12 12:54:43 +0100

    First commit

---
+++ fizzBuzz/README.md
@@ -0,0 +1,3 @@
+First kata - Fizz Buzz.
+
+Trying to get it done in 5 mins :-/
---
+++ fizzBuzz/spec/fizz_buzz_spec.rb
@@ -0,0 +1,17 @@
+require 'fizz_buzz'
+
+describe 'fizz_buzz' do
+
+  it 'should know that three is divisible by three' do
+    expect(is_divisible_by_three?(3)).to be true
+  end
+
+  it 'should know that four is not divisible by three' do
+    expect(is_divisible_by_three?(4)).to be false
+  end
+
+  it 'should know that five is divisible by five' do
+    expect(is_divisible_by_five?(5)).to be true
+  end
+
+end
