d35406b6f1f282a41b9efa408f2ca70af06a151d
ruthearle <rrearle@gmail.com>
2014-08-14 11:12:31 +0100

    test passed - expectation met

--- fizzBuzz2/lib/fizz_buzz.rb
+++ fizzBuzz2/lib/fizz_buzz.rb
@@ -17,6 +17,7 @@ def is_divisible_by?(number, divisor)
 end
 
 def fizz_buzz(number)
+  return "FizzBuzz" if is_divisible_by_fifteen?(number)
   return "Fizz" if is_divisible_by_three?(number)
   return "Buzz" if is_divisible_by_five?(number)
 end
