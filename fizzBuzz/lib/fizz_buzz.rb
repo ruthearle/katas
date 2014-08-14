bfc0c40e3e1f00262c3a221d3fdfe3ed2c092e1c
ruthearle <rrearle@gmail.com>
2014-08-14 10:36:31 +0100

    wrote and passed first test

--- fizzBuzz/lib/fizz_buzz.rb
+++ fizzBuzz/lib/fizz_buzz.rb
@@ -1,26 +1,21 @@
 # FizzBuzz game code.
 #
-# Method to find numbers divisible by three
 def is_divisible_by_three?(number)
   is_divisible_by?(number, 3)
 end
 
-# Method to find numbers divisible by five
 def is_divisible_by_five?(number)
   is_divisible_by?(number, 5)
 end
 
-# Method to find numbers divisible by fifteen
 def is_divisible_by_fifteen?(number)
   is_divisible_by?(number, 15)
 end
 
-# Method that holds the logic of the game
 def is_divisible_by?(number, divisor)
   number % divisor == 0
 end
 
-# Method that returns 'Fizz'
 def fizz_buzz(number)
   return "FizzBuzz" if is_divisible_by_fifteen?(number)
   return "Fizz" if is_divisible_by_three?(number)
